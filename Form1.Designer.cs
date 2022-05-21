f523833f0600f421a2ff6f21ed0818970acf5677
TheoEMT <theogame999@gmail.com>
2022-05-21 20:21:43 +0200

    project init

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace perk_roulette_NET6._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tueurButton = new System.Windows.Forms.Button();
+            this.survivantButton = new System.Windows.Forms.Button();
+            this.perk1 = new System.Windows.Forms.Label();
+            this.perk2 = new System.Windows.Forms.Label();
+            this.perk3 = new System.Windows.Forms.Label();
+            this.perk4 = new System.Windows.Forms.Label();
+            this.perkPic1 = new System.Windows.Forms.PictureBox();
+            this.perkPic2 = new System.Windows.Forms.PictureBox();
+            this.perkPic3 = new System.Windows.Forms.PictureBox();
+            this.perkPic4 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.perkPic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perkPic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perkPic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perkPic4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tueurButton);
+            this.panel1.Controls.Add(this.survivantButton);
+            this.panel1.Location = new System.Drawing.Point(12, 518);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 112);
+            this.panel1.TabIndex = 0;
+            // 
+            // tueurButton
+            // 
+            this.tueurButton.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tueurButton.Location = new System.Drawing.Point(20, 10);
+            this.tueurButton.Name = "tueurButton";
+            this.tueurButton.Size = new System.Drawing.Size(314, 34);
+            this.tueurButton.TabIndex = 1;
+            this.tueurButton.Text = "Tueur";
+            this.tueurButton.UseVisualStyleBackColor = true;
+            this.tueurButton.Click += new System.EventHandler(this.tueurButton_Click);
+            // 
+            // survivantButton
+            // 
+            this.survivantButton.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.survivantButton.Location = new System.Drawing.Point(20, 67);
+            this.survivantButton.Name = "survivantButton";
+            this.survivantButton.Size = new System.Drawing.Size(314, 34);
+            this.survivantButton.TabIndex = 1;
+            this.survivantButton.Text = "Survivant";
+            this.survivantButton.UseVisualStyleBackColor = true;
+            this.survivantButton.Click += new System.EventHandler(this.survivantButton_Click);
+            // 
+            // perk1
+            // 
+            this.perk1.AutoSize = true;
+            this.perk1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perk1.Location = new System.Drawing.Point(28, 45);
+            this.perk1.Name = "perk1";
+            this.perk1.Size = new System.Drawing.Size(69, 24);
+            this.perk1.TabIndex = 1;
+            this.perk1.Text = "perkS1";
+            // 
+            // perk2
+            // 
+            this.perk2.AutoSize = true;
+            this.perk2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perk2.Location = new System.Drawing.Point(28, 157);
+            this.perk2.Name = "perk2";
+            this.perk2.Size = new System.Drawing.Size(69, 24);
+            this.perk2.TabIndex = 2;
+            this.perk2.Text = "perkS2";
+            // 
+            // perk3
+            // 
+            this.perk3.AutoSize = true;
+            this.perk3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perk3.Location = new System.Drawing.Point(28, 271);
+            this.perk3.Name = "perk3";
+            this.perk3.Size = new System.Drawing.Size(69, 24);
+            this.perk3.TabIndex = 3;
+            this.perk3.Text = "perkS3";
+            // 
+            // perk4
+            // 
+            this.perk4.AutoSize = true;
+            this.perk4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perk4.Location = new System.Drawing.Point(28, 387);
+            this.perk4.Name = "perk4";
+            this.perk4.Size = new System.Drawing.Size(69, 24);
+            this.perk4.TabIndex = 4;
+            this.perk4.Text = "perkS4";
+            // 
+            // perkPic1
+            // 
+            this.perkPic1.Location = new System.Drawing.Point(546, 22);
+            this.perkPic1.Name = "perkPic1";
+            this.perkPic1.Size = new System.Drawing.Size(100, 86);
+            this.perkPic1.TabIndex = 6;
+            this.perkPic1.TabStop = false;
+            // 
+            // perkPic2
+            // 
+            this.perkPic2.Location = new System.Drawing.Point(546, 132);
+            this.perkPic2.Name = "perkPic2";
+            this.perkPic2.Size = new System.Drawing.Size(100, 86);
+            this.perkPic2.TabIndex = 7;
+            this.perkPic2.TabStop = false;
+            // 
+            // perkPic3
+            // 
+            this.perkPic3.Location = new System.Drawing.Point(546, 246);
+            this.perkPic3.Name = "perkPic3";
+            this.perkPic3.Size = new System.Drawing.Size(100, 86);
+            this.perkPic3.TabIndex = 8;
+            this.perkPic3.TabStop = false;
+            // 
+            // perkPic4
+            // 
+            this.perkPic4.Location = new System.Drawing.Point(546, 369);
+            this.perkPic4.Name = "perkPic4";
+            this.perkPic4.Size = new System.Drawing.Size(100, 86);
+            this.perkPic4.TabIndex = 9;
+            this.perkPic4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 642);
+            this.Controls.Add(this.perk4);
+            this.Controls.Add(this.perk1);
+            this.Controls.Add(this.perk3);
+            this.Controls.Add(this.perk2);
+            this.Controls.Add(this.perkPic4);
+            this.Controls.Add(this.perkPic3);
+            this.Controls.Add(this.perkPic2);
+            this.Controls.Add(this.perkPic1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Perk Roulette";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.perkPic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perkPic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perkPic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perkPic4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button tueurButton;
+        private System.Windows.Forms.Button survivantButton;
+        private System.Windows.Forms.Label perk1;
+        private System.Windows.Forms.Label perk2;
+        private System.Windows.Forms.Label perk3;
+        private System.Windows.Forms.Label perk4;
+        private System.Windows.Forms.PictureBox perkPic1;
+        private System.Windows.Forms.PictureBox perkPic2;
+        private System.Windows.Forms.PictureBox perkPic3;
+        private System.Windows.Forms.PictureBox perkPic4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace perk_roulette_NET6._0
+{
+    public partial class Form1 : Form
+    {
+        private string[] perkKiller = new string[] { "Agitation", "Amasseur", "Appel de la Mer", "Aucune Issue", "Boucher Sadique", "Bricolo", "Chili et Barbecue", "Claustrophobie",
+        "Coulrophobie", "Coup de Grâce", "Coup du Pendu", "Cran dément", "Crochet Flagellateur: Don de souffrance", "Corchet Flagellateur: Flots de Rage", "Crochet Flagellateur: Echo de Douleur",
+        "Discordance", "Dispositif de l'Homme Mort", "Déboussoler", "Ennemi Juré", "Espions de l'Ombre", "Fais ton Choix", "Fascination", "Force Brute", "Frissons Palpitants", "Fureur de l'Esprit",
+        "Garder le Meilleur pour la Fin", "Gardien de Sang", "Hystérie", "Impasse", "Implacable", "Inquiétant", "Insidieux", "Instinct Territorial", "Intensité Ardente", "Intervention Impure",
+        "Je vous Ecoute", "Jouer avec la Nourriture", "K.O", "Le Tour est joué", "Lien Mortel", "Limier", "Lumière Mourante", "Murmure Amer", "Murmures", "Opprssion", "Peur Contagieuse",
+        "Piste de Souffrance", "Pisteur", "Poing de Fer", "Poursuite Furtive", "Poursuivant Meurtrier", "Prise du Dragon", "Prédateur", "Prédation", "Présence Perturbante", "Présence Ecrasante",
+        "Pénitence Forcée", "Rancoeur", "Rejeton de l'Obscurité", "Rejeton de Lumière", "Réparateur", "Sanctuaire Monstrueux", "Secousse", "Semeur de Peur", "Sombre Dévotion", "Sombre Etreinte",
+        "Hex: Berceuse de la Chasseuse", "Hex: Châtiment", "Hex: Contrôle des Foules", "Hex: Euphorie de la Chasse", "Hex: Faveur de Sang", "Hex: Immortel", "Hex: Jouet", "Hex: Le Troisième Sceau",
+        "Hex: Mangeur d'Espoir", "Hex: Personne n'échappe à la Mort", "Hex: Repentir", "Hex: Ruine", "Hex: Terrain Hanté", "Souviens-Toi de Moi", "Stridor", "Surcharge", "Surveillance", "Surveillance et Maltraitance",
+        "Tactique de Zanshin", "Tempête Impitoyable", "Tenace", "Thanatophobie", "Trépas de Franklin", "Vierge de Fer", "Vocation de l'Infirmière", "Echo Sanguin", "Eruption"};
+
+        private string[] perkSurvivor = new string[] { "Abandonné", "Accrochez-Vous", "Adrénaline", "Affinités", "Alliance Refoulée", "Assurance", "Atout de la Manche", "Atterrissage Equilibré",
+        "Augmenter la Mise", "Auto-Traitement", "Autodidacte", "Bras de Fer", "Bénédiction: Cercle de Soins", "Bénédiction: Exponentiel", "Bénédiction: Marche de l'Ombre", "Bénédiction: Théorie Ténébreuse",
+        "Ce n'est pas en Train d'Arriver", "Clairvoyance", "Concentration Intérieure", "Confidentiel", "Connaissance en Botanique", "Connexion Empathique", "Conscience Situationnelle", "Conscience de Soi",
+        "Conseils Parentaux", "Contre-Force", "Conçu pour Durer", "Coup Décisif", "Coupe de Chance", "Course Effrénée", "Danse Avec Moi", "De Front", "Debout !", "Dernier Survivant", "Distorsion",
+        "Diversion", "Dead Hard", "Débrouillardise", "Déjà-Vu", "Détermination du Bleu", "Effondrement", "Effusion", "Empathie", "En Planque", "Espoir", "Esprit Calme", "Estimation", "Excès de Zèle",
+        "Montrez ce que vous savez Faire", "Force de Caractère", "Frisson", "Gardien", "Grenade Aveuglante", "Guérison Interne", "Indéfectible", "Instinct de Pillard", "Instinct de Survie",
+        "Intuition de l'Inspecteur", "Le Coeur sur la Main", "Leurre", "Libération", "Lien", "Manifeste Résiduel", "Meneur", "Mesure de Correction", "Mesures Désespérées", "Mine Explosive", 
+        "Nous Vivrons Eternellement", "Nous Y Arriverons", "Objet d'Obsession", "On n'Abandonne Personne", "Pacte de Sang", "Par Tous les Moyens", "Parenté", "Parfaite Occasion", "Pas Envie", 
+        "Petit Gibier", "Pharmacie", "Poids Plume", "Pour le Peuple", "Protection de l'Ame", "Prémonition", "Rapide et Silencieux", "Renaissance", "Renouvellement", "Résistance", "Saboteur", 
+        "Sentiment Obscure", "Serrer les Dents", "Solidarité", "Souple", "Succès Ecrasant", "Suivi des Soins", "Supercherie", "Surmonter", "Sursis", "Technicien", "Ténacité", "Veillée",
+        "Viande Glissante", "Vigilant", "Visionnaire", "Voie Rapide", "Volonté de Fer", "A la Revoyure", "Evasion", "Evasion Urbaine"};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tueurButton_Click(object sender, EventArgs e)
+        {
+            Random alea = new Random();
+
+            //tirage des 4 perks avec possibilité de doublon
+
+            int perkNumber1 = alea.Next(perkKiller.Length);
+            string perkText1 = perkKiller[perkNumber1];
+
+
+            int perkNumber2 = alea.Next(perkKiller.Length);
+            string perkText2 = perkKiller[perkNumber2];
+            
+            int perkNumber3 = alea.Next(perkKiller.Length);
+            string perkText3 = perkKiller[perkNumber3];
+
+            int perkNumber4 = alea.Next(perkKiller.Length);
+            string perkText4 = perkKiller[perkNumber4];
+
+            //while pour éviter de tomber plusieurs fois sur la même perks
+
+            while(perkNumber2 == perkNumber1)
+            {
+                perkNumber2 = alea.Next(perkKiller.Length);
+                perkText2 = perkKiller[perkNumber2];
+            }
+
+            while(perkNumber3 == perkNumber1 || perkNumber3 == perkNumber2)
+            {
+                perkNumber3 = alea.Next(perkKiller.Length);
+                perkText3 = perkKiller[perkNumber3];
+            }
+
+            while(perkNumber4 == perkNumber1 || perkNumber4 == perkNumber2 || perkNumber4 == perkNumber3)
+            {
+                perkNumber4 = alea.Next(perkKiller.Length);
+                perkText4 = perkKiller[perkNumber4];
+            }
+
+            //Affichage des perks
+
+            perk1.Text = "" + perkText1;
+            perk2.Text = "" + perkText2;
+            perk3.Text = "" + perkText3;
+            perk4.Text = "" + perkText4;
+        }
+
+        private void survivantButton_Click(object sender, EventArgs e)
+        {
+            Random alea = new Random();
+
+            //tirage des 4 perks avec possibilité de doublon
+
+            int perkNumber1 = alea.Next(perkSurvivor.Length);
+            string perkText1 = perkSurvivor[perkNumber1];
+
+            int perkNumber2 = alea.Next(perkSurvivor.Length);
+            string perkText2 = perkSurvivor[perkNumber2];
+
+            int perkNumber3 = alea.Next(perkSurvivor.Length);
+            string perkText3 = perkSurvivor[perkNumber3];
+
+            int perkNumber4 = alea.Next(perkSurvivor.Length);
+            string perkText4 = perkSurvivor[perkNumber4];
+
+            //while pour éviter de tomber plusieurs fois sur la même perks
+
+            while (perkNumber2 == perkNumber1)
+            {
+                perkNumber2 = alea.Next(perkSurvivor.Length);
+                perkText2 = perkSurvivor[perkNumber2];
+            }
+
+            while (perkNumber3 == perkNumber1 || perkNumber3 == perkNumber2)
+            {
+                perkNumber3 = alea.Next(perkSurvivor.Length);
+                perkText3 = perkSurvivor[perkNumber3];
+            }
+
+            while (perkNumber4 == perkNumber1 || perkNumber4 == perkNumber2 || perkNumber4 == perkNumber3)
+            {
+                perkNumber4 = alea.Next(perkSurvivor.Length);
+                perkText4 = perkSurvivor[perkNumber4];
+            }
+
+            //Affichage des perks
+
+            perk1.Text = "" + perkText1;
+            perk2.Text = "" + perkText2;
+            perk3.Text = "" + perkText3;
+            perk4.Text = "" + perkText4;
+
+        }
+    }
+}
